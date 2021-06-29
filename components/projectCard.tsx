d006cfbeda4cb7555185b86372037267a1762226
Tweaka <vzxpk@yahoo.fr>
2021-06-29 11:59:32 +0200

    updated component

--- components/projectCard.tsx
+++ components/projectCard.tsx
@@ -1,7 +1,7 @@
 export default function Card({ projects }) {
   return projects.map((project, index) => {
     return (
-      <div className="flex flex-wrap">
+      <div className="flex flex-wrap ">
         <img className="rounded-tr-sm w-60 h-52" src={project.image}></img>
         <h1> {project.name} </h1>
       </div>
