270d8188931c17437c1930c1b11d86664b41def3
Tweaka <vzxpk@yahoo.fr>
2021-06-30 07:14:39 +0200

    started creating dashboard interface

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,31 @@ interface IProps {
 
 function Home({ projects, categories, levels }: IProps): JSX.Element {
   return (
-    <div>
+    <div className="bg-gray-100 px-28">
+      <div className="flex items-center">
+        <div className="w-1/4 h-40 border m-2 bg-white flex items-center justify-between">
+          <span className="m-2 bg-Fiverr px-2 text-white text-7xl font-bold rounded-full w-24 h-24 border border-gray-400 flex items-center justify-center">
+            <button className="">T</button>
+          </span>
+          <ul className="text-blue-700 font-bold text-md mr-2">
+            <li>View My Favorites</li>
+            <li className="py-2">Invite Friends</li>
+            <li>Start Selling</li>
+          </ul>
+        </div>
+        <div className="flex flex-col">
+          <div className="flex ">
+            <ul className="flex border-b-2 text-gray-600 w-full mb-3">
+              <li className="mx-2 ">
+                <button className="font-bold"> CURRENT PROJECTS </button>
+              </li>
+              <li className="mx-2">TO-DOS</li>
+              <li className="mx-2">NOTIFICATIONS</li>
+            </ul>
+          </div>
+          <div className="bg-white"> coucou je suis un projet </div>
+        </div>
+      </div>
       <div>
         <pre>{JSON.stringify(categories, null, 2)}</pre>
       </div>
